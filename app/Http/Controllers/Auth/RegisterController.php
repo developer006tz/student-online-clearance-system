e8d9ad07528aceed1736250b2eff94975105c41d
Ludovick Konyo <developer.ludovic@gmail.com>
2023-06-26 09:00:35 +0000

    add

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,6 @@ class RegisterController extends Controller
             return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
             'role'=>['required','unique:users'],
         ]);
         }else{
@@ -113,7 +112,7 @@ class RegisterController extends Controller
         }
 
         $role = $data['role'];
-
+        
         if ($role == 'student') {
             $role = 'Student';
             $user->assignRole(Role::findByName('student'));
